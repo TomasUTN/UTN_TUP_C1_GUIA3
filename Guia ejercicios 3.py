46cbcdf72025f4e8234b2948f76975b64aaa3102
TomasUTN <Tomi.miranda272@gmail.com>
2023-09-03 15:38:02 -0300

    version 1

---
+++ Guia ejercicios 3.py
@@ -0,0 +1,23 @@
+# Entiendo en el problema que las secretarias deberian cargar los datos para otorgarle la receta
+# pedido de datos para los clientes
+print("Muy buenos dias")
+
+NombPa = input("Ingrese el nombre del paciente: ")
+Nac = input("Ingrese la fecha de nacimiento del paciente(en formato dd/mm/aaaa): ")
+Med = input("Ingrese la droga recetada por el medico: ")
+Dosis = input("Ingrese la dosis de " + Med + " recetadas por el medico: ")
+Uso = input("Ingrese la forma de uso de la droga recetada: " + Med + ": ")
+
+
+# Lo que mostrara el ticket que se lleva dicho paciente
+print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+print("Paciente: " + NombPa)
+print("Fecha de nacimiento: " + Nac)
+print("Esta receta es valida para la compra/uso de " + Med)
+print("Dicho medicamento sera utilizado en la cantidad: "+ Dosis)
+print("Con modalidad de uso: " + Uso)
+
+
+print("Atte clinica medica Sanatorio de la cañada - Larrabure 1466, X5900 Villa María, Córdoba")
+print("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+
